256830f0dc63db41dbd6ea75585ad8f0923a79b2
FranForge <fjesus.fjhh@gmail.com>
2025-11-12 23:15:49 +0100

    feat: Nueva pagina creandose.

---
+++ newpage/scripts/attributesDisplayer.js
@@ -0,0 +1,20 @@
+//Obtiene todos los contenedores de atributos.
+const attributeContainers = document.querySelectorAll('.attributes-container');
+
+//Recorre todos los contenedores de atributos y agrega los atributos como tarjetas.
+for (let i = 0; i < attributeContainers.length; i++)
+{
+    const container = attributeContainers[i];
+
+    //Obtiene los atributos del contenedor.
+    const attributes = container.dataset.attributes.split(',');
+
+    //Recorre todos los atributos y crea un div por cada uno.
+    for (let j = 0; j < attributes.length; j++)
+    {
+        const card = document.createElement('div');
+        card.className = 'attribute-card';
+        card.textContent = attributes[j];
+        container.appendChild(card);
+    }
+}
---
+++ newpage/scripts/rotateTexts.js
@@ -0,0 +1,40 @@
+//Obtiene todos los textos que contengan rotating-text class.
+const rotateTextSpans = document.querySelectorAll('.rotating-text');
+
+//Recorre todos los elementos de rotateTextSpans para mostrarlos.
+for (let i = 0; i < rotateTextSpans.length; i++)
+{
+    const span = rotateTextSpans[i];
+    const texts = span.dataset.texts.split(',');
+    let index = 0;
+    let letterIndex = 0;
+
+    //Escribe las letras.
+    function typeLetter() {
+        const text = texts[index];
+        span.textContent += text[letterIndex];
+        letterIndex++;
+
+        //Si las letras que se han escrito son mayores se pasa al siguiente texto.
+        if (letterIndex >= text.length) 
+        {
+            // Espera antes de pasar al siguiente texto.
+            setTimeout(nextText, 1500);
+        } 
+        //Si no escribe de nuevo la letra.
+        else
+        {
+            setTimeout(typeLetter, 100);
+        }
+    }
+
+    // Función que prepara el siguiente texto
+    function nextText() {
+        index = (index + 1) % texts.length;
+        letterIndex = 0;
+        span.textContent = "";
+        typeLetter();
+    }
+
+    typeLetter();
+}
