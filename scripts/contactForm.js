a1aa9ade85df4d3429f5dae4d242b6b0112aae48
FranForge <fjesus.fjhh@gmail.com>
2025-11-24 21:57:42 +0100

    feat: Agregado formulario con API REST propia en render

---
+++ scripts/contactForm.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const form = document.getElementById("contact-form");
+    console.log("Form encontrado:", form);
+
+    form.addEventListener("submit", async (e) => {
+
+        console.log("Submit ejecutado");
+
+        //Evita que recargue la web.
+        e.preventDefault();
+
+        //Crea el formulario.
+        const formData = new FormData(form);
+
+        try {
+            const result = await fetch("https://api.franforge.es/api/ContactForm/send", {
+                method: "POST",
+                body: formData
+            });
+
+            if (!result.ok) {
+                const text = await result.text();
+                console.error("Respuesta no OK:", result.status, text);
+                return;
+            }
+
+            console.log("Formulario enviado correctamente");
+
+        } catch (err) {
+            console.error("Error enviando formulario");
+        }
+    });
+});
