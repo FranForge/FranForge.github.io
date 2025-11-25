56faa797e5719f7802fe9f51a81dae1fd47204e8
FranForge <fjesus.fjhh@gmail.com>
2025-11-25 11:46:47 +0100

    feat: Ahora al enviar el formulario se obtiene una respuesta bajo el mismo

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -1,3 +1,4 @@
+//Espera a que el DOM se haya cargado.
 document.addEventListener("DOMContentLoaded", () => {
 
     const form = document.getElementById("contact-form");
@@ -23,13 +24,47 @@ document.addEventListener("DOMContentLoaded", () => {
             if (!result.ok) {
                 const text = await result.text();
                 console.error("Respuesta no OK:", result.status, text);
+                displayError(text);
                 return;
             }
 
             console.log("Formulario enviado correctamente");
+            displaySuccess();
 
         } catch (err) {
             console.error("Error enviando formulario");
+            displayError(error);
         }
     });
 });
+
+//Muestra la confirmacion en pantalla.
+function displaySuccess()
+{
+    const successContainer = document.getElementById("contact-submit-successfully-message");
+    if(successContainer == undefined) return;
+
+    successContainer.classList.add("show");
+
+    setTimeout(() => hideMessage(successContainer), 5000);
+}
+
+//Muestra el error en pantalla ademas de la advertencia.
+function displayError(errorText)
+{
+    const errorContainer = document.getElementById("contact-submit-failed-message");
+    if(errorContainer == undefined) return;
+
+    errorContainer.classList.add("show");
+
+    const errorMessage = document.getElementById("contact-submit-failed-error");
+    errorMessage.textContent = errorText;
+
+    setTimeout(() => hideMessage(errorContainer), 5000);
+}
+
+//Espera x segundos y oculta el mensaje.
+function hideMessage(element)
+{
+    element.classList.remove("show");
+}
