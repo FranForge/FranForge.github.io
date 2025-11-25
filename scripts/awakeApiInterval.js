ca2d7801cfa18993664cbc1e101859bf4438224e
FranForge <fjesus.fjhh@gmail.com>
2025-11-25 12:09:39 +0100

    feat: Ahora hace ping nada mas cargar la pagina

--- scripts/awakeApiInterval.js
+++ scripts/awakeApiInterval.js
@@ -1,9 +1,14 @@
-//Manda un ping cada x tiempo a la api para activarla.
-setInterval(() => {
 
+//Realiza un Ping a la Api para activarla.
+function pingApi()
+{
     fetch("https://api.franforge.es/api/ping")
         .catch(() => {
 
         });
+}
 
-}, 5 * 60 * 1000); //Se ejecuta cada 5 minutos mientras esta navegando el usuario.
+pingApi();
+
+//Manda un ping cada x tiempo a la api para activarla.
+setInterval(pingApi, 5 * 60 * 1000);
